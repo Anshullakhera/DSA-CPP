39324e0dce31b9d7c90b198779d4e56d0359d251
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-10-18 18:30:51 +0530

    Update Minimal Cost.cpp

--- DP/Minimal Cost.cpp
+++ DP/Minimal Cost.cpp
@@ -1,4 +1,14 @@
-int n = arr.size();
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n = arr.size();
         vector<int> dp(n, INT_MAX); 
         dp[0] = 0; 
 
@@ -9,3 +19,34 @@ int n = arr.size();
         }
 
         return dp[n - 1]; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minimizeCost(k, arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
