c28aac9880a1e9d836d6634696ce43bfa2e30553
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-10-25 23:05:07 +0530

    Create Alternative Sorting.cpp

---
+++ Array/Alternative Sorting.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        sort(arr.begin(), arr.end());
+        vector<int> ans;
+        int n = arr.size();
+        int i = 0, j = n - 1;
+        while(i < j)
+        {
+            ans.push_back(arr[j]);
+            ans.push_back(arr[i]);
+            i++;
+            j--;
+        }
+        if(n % 2 == 1)
+        {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
