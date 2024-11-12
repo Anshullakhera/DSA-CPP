7df3379b49f86833b3086824388430e1c944be02
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-11-12 21:18:16 +0530

    Create Meeting Rooms.cpp

---
+++ Array/Meeting Rooms.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Your Code Here
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i][0]==arr[i+1][0]){
+                return false;
+            }
+            else if(arr[i][1]>arr[i+1][0]){
+                return false;
+            }
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            arr[i].push_back(x);
+            arr[i].push_back(y);
+        }
+        cin.ignore();
+        Solution obj;
+        bool ans = obj.canAttend(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
