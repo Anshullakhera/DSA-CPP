ef82f24ae7472d197f83ec1ca3f7bc3901545360
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-11-25 15:10:41 +0530

    Create Maximum Product Subarray.cpp

---
+++ Array/Maximum Product Subarray.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int n = arr.size();
+        int pre = 1, suf = 1;
+        int maxi = INT_MIN;
+        for( int i = 0; i < n; i++){
+            if(pre == 0) pre = 1;
+            if(suf == 0) suf = 1;
+            pre *= arr[i];
+            suf *= arr[n-i-1];
+            maxi = max(maxi, max( pre, suf));
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
