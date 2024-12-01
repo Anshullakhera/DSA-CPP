beeeb112d851345396a623d5916baa355c4914ca
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-12-01 21:21:45 +0530

    Create Non Repeating Character.cpp

---
+++ String/Non Repeating Character.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        unordered_map<char, int> mp;
+        for(char c: s){
+            mp[c]++;
+        }
+        for(char c: s){
+            if(mp[c] == 1){
+                return c;
+            }
+        }
+        return '$';
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
