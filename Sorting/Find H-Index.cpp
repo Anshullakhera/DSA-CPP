4504ae491686d808bdf11e2d665e5ed5012f1ad1
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-12-06 21:53:35 +0530

    Create Find H-Index.cpp

---
+++ Sorting/Find H-Index.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find hIndex
+    int hIndex(vector<int>& citations) {
+        // code here
+        sort(citations.begin(),citations.end());
+        int32_t ans = 0;
+        int32_t n = citations.size();
+        for (int32_t i = n-1; i >= 0; --i){
+            ans  = max(ans, min(n - i, citations[i]));
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        int result = ob.hIndex(arr);
+
+        cout << result << endl;
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
