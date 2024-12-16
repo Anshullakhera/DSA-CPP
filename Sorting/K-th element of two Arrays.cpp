2e797e48963f7583677b7110ef12394b58fc518f
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-12-16 19:07:59 +0530

    Create K-th element of two Arrays.cpp

---
+++ Sorting/K-th element of two Arrays.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        // code here
+        vector<int> v;
+        for(int i=0;i<a.size();i++)
+        {
+            v.push_back(a[i]);
+        }
+        for(int i=0;i<b.size();i++)
+        {
+            v.push_back(b[i]);
+        }
+        sort(v.begin(),v.end());
+        
+        for(int i=0;i<v.size();i++)
+        {
+            if(i==k-1)
+            return v[i];
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
