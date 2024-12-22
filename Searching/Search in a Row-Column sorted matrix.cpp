97029ad8b7c5c0f09e3540ff9b3b20d9401a672f
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2024-12-22 19:05:08 +0530

    Create Search in a Row-Column sorted matrix.cpp

---
+++ Searching/Search in a Row-Column sorted matrix.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+        int i=mat[0].size()-1,j=0;
+        while(i>=0 && j<mat.size()){
+            if(mat[j][i]==x) return true;
+            if(mat[j][i]>x) i--;
+            else j++;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        int x;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        cin >> x;
+        Solution ob;
+        bool an = ob.matSearch(matrix, x);
+        if (an)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
