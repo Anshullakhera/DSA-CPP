b473e7d665c6a818df575596a08442a0f1aa0e31
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-01-03 23:19:01 +0530

    Create Count Subarrays with given XOR.cpp

---
+++ Array/Hash/Count Subarrays with given XOR.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+        map<int, int> mp;
+
+        int sum = 0;
+        long long ans = 0;
+
+        for(int i = 0; i < arr.size(); ++i) {
+            sum ^= arr[i]; 
+
+            if (sum == k) {
+                ++ans; 
+            }
+
+            if (mp.find(sum ^ k) != mp.end()) {
+                ans += mp[sum ^ k]; 
+            }
+
+            mp[sum]++;
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore(); // Ignore the newline after the number of test cases
+
+    while (tc-- > 0) {
+        string input;
+        getline(cin, input); // Read the whole line for the array
+
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+
+        while (ss >> num) {
+            arr.push_back(num); // Push numbers into the vector
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore(); // Ignore the newline after k
+
+        Solution obj;
+        cout << obj.subarrayXor(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
