1409165e187dc417f74b35e244cb73e9847dfc78
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-02-22 09:46:19 +0530

    Create Longest valid Parentheses sol2.cpp

---
+++ Stack/Longest valid Parentheses sol2.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxLength(string& s) {
+        // code here
+         stack<int> st;
+    st.push(-1);  
+    int maxLen = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if (!st.empty()) {
+                maxLen=max(maxLen,i-st.top());
+            } else {
+                st.push(i);  
+            }
+        }
+    }
+    return maxLen;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.maxLength(S) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
