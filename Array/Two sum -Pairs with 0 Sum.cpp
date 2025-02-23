b97de4a779fd88b5fc956d5904fa1137e94a27fa
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-02-23 14:08:11 +0530

    Create Two sum -Pairs with 0 Sum.cpp

---
+++ Array/Two sum -Pairs with 0 Sum.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+        // code here
+    set<pair<int, int>> ret;  
+    sort(arr.begin(), arr.end());  
+
+    int left = 0, right = arr.size() - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == 0) {
+            ret.insert({arr[left], arr[right]});
+            left++;
+            right--;  
+        } else if (sum < 0) {
+            left++;  
+        } else {
+            right--; 
+        }
+    }
+
+    vector<vector<int>> result;
+    for (const auto& p : ret) {
+        result.push_back({p.first, p.second});
+    }
+
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function Template for C++
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        // char c;cin>>c;
+        Solution obj;
+        vector<vector<int>> res = obj.getPairs(arr);
+        if (res.size() == 0) {
+            cout << endl;
+        } else
+            Matrix::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
