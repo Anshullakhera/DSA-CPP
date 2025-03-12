9dcd338ff482795b8833d0c3dfa799d6e3e60ce6
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-03-12 21:04:50 +0530

    Create Min Cost Climbing Stairs.cpp

---
+++ DP/Min Cost Climbing Stairs.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        // Write your code here
+        int prev2=cost[0],prev1=cost[1];
+       
+       for(int i=2;i<cost.size();i++){
+           int newCost = cost[i] + min(prev2,prev1);
+            prev2=prev1;
+            prev1=newCost;
+       }
+       
+       return min(prev2,prev1);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t-- > 0) {
+        string str;
+        getline(cin, str);
+
+        stringstream ss(str);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution sln;
+        int res = sln.minCostClimbingStairs(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
