f5e73b4d987f1668956f3e5e6292ccd1ee28aaa4
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-04-14 21:07:02 +0530

    Create Alien Dictionary.cpp

---
+++ Graph/Alien Dictionary.cpp
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string findOrder(vector<string> &words) {
+        // code here
+        int n = words.size();
+        vector<vector<int>> adj(26);
+        vector<bool> p(26, false);
+        vector<int>indegree(26, 0);
+        for(auto &word: words){
+            for(char c: word){
+                p[c-'a'] = 'true';
+            }
+        }
+        for(int i = 0; i< n-1; i++){
+            string str1 = words[i];
+            string str2 = words[i+1];
+            int len = min(str1.size(), str2.size());
+            bool check = false;
+            for(int  ind = 0; ind<len; ind++){
+                if(str1[ind] != str2[ind]){
+                    adj[str1[ind] -'a'].push_back(str2[ind] - 'a');
+                    indegree[str2[ind]-'a']++;
+                    check = true;
+                    break;
+                }
+            }
+            if(!check && str1.length() > str2.length()){
+                return "";
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i< 26; i++){
+            if(p[i] && indegree[i]==0){
+                q.push(i);
+            }
+        }
+        string ans = "";
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans += char(node +'a');
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        for(int i = 0; i<26; i++){
+            if(p[i] && indegree[i]>0){
+                return "";
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+bool validate(const vector<string> &original, const string &order) {
+    unordered_map<char, int> mp;
+    for (const string &word : original) {
+        for (const char &ch : word) {
+            mp[ch] = 1;
+        }
+    }
+    for (const char &ch : order) {
+        if (mp.find(ch) == mp.end())
+            return false;
+        mp.erase(ch);
+    }
+    if (!mp.empty())
+        return false;
+
+    for (int i = 0; i < order.size(); i++) {
+        mp[order[i]] = i;
+    }
+
+    for (int i = 0; i < original.size() - 1; i++) {
+        const string &a = original[i];
+        const string &b = original[i + 1];
+        int k = 0, n = a.size(), m = b.size();
+        while (k < n and k < m and a[k] == b[k]) {
+            k++;
+        }
+        if (k < n and k < m and mp[a[k]] > mp[b[k]]) {
+            return false;
+        }
+        if (k != n and k == m) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    int t = stoi(str);
+    while (t--) {
+        getline(cin, str);
+        stringstream ss(str);
+        string curr;
+        vector<string> words;
+        while (ss >> curr)
+            words.push_back(curr);
+
+        vector<string> original = words;
+
+        Solution ob;
+        string order = ob.findOrder(words);
+
+        if (order.empty()) {
+            cout << "\"\"" << endl;
+        } else {
+            cout << (validate(original, order) ? "true" : "false") << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
