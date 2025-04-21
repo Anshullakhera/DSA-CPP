9ed3a16a9a4a191011677078f77b857c131dbb8d
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-04-21 22:32:18 +0530

    Create First positive Missing in Array.cpp

---
+++ Array/First positive Missing in Array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+        int n=arr.size()+1;
+        long long total = 1LL * n * (n + 1) / 2;
+        long long sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return (int)(total - sum);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
