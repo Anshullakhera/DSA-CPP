bdb5396ec8f4900dbc5b304aab2f91e460fafee3
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-04-23 08:23:26 +0530

    Create Unique Number II.cpp

---
+++ Array/Map/Unique Number II.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> singleNum(vector<int>& arr) {
+        // Code here.
+        vector<int> ans;
+        map<int, int> mp;
+        for(it:arr){
+            mp[it]++;
+        }
+        for(int it : arr){
+            if(mp[it]== 1){
+                ans.push_back(it);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.singleNum(arr);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
