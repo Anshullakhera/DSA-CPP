5378b4d07a1a8d865e659b6ddcdd1bcc8a5cb1ed
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-05-30 22:04:28 +0530

    Create Closest Neighbour in BST.cpp

---
+++ Tree/Closest Neighbour in BST.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+  int ans=-1;
+  void Solve(Node* root,int k){
+      if(root==NULL)
+      return;
+      if(root->data<=k){
+          ans=root->data;
+          Solve(root->right,k);
+      }
+      else
+      Solve(root->left,k);
+  }
+    int findMaxFork(Node* root, int k) {
+        
+        Solve(root,k);
+        return ans;
+    }
+};
