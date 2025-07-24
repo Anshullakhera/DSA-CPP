880b04abe6ece26a2712226257bc6e308dd40fde
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-07-24 21:17:44 +0530

    Create Last Moment Before All Ants Fall Out.cpp

---
+++ Greedy Algorithm/Last Moment Before All Ants Fall Out.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int mini= INT_MAX;
+     int maxi= 0;
+     if(!left.empty()){
+     for (int i=0; i< left.size(); i++){
+         maxi= max(maxi, left[i]);
+     }
+     }
+     if(!right.empty()){
+        for (int i=0; i< right.size(); i++){
+            mini= min(mini, right[i]);
+        } 
+     }
+     return max(maxi, n- mini);
+        
+    }
+};
