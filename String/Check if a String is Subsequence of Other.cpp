a81a98fb7ff3f8ba04b36e421db1aac6226229f6
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-08-26 20:46:38 +0530

    Create Check if a String is Subsequence of Other.cpp

---
+++ String/Check if a String is Subsequence of Other.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isSubSeq(string& s1, string& s2) {
+        // code here
+        int i = 0, j = 0;
+
+    while (i < s1.size() && j < s2.size()) {
+        if (s1[i] == s2[j]) {
+            i++;  // move in s if match found
+        }
+        j++;  // always move in t
+    }
+
+    return i == s1.size();
+    }
+};
