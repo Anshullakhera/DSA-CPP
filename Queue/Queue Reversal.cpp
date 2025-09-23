c9a3b731a26fa779a3c425209696bc9e795eca19
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-09-23 21:01:38 +0530

    Create Queue Reversal.cpp

---
+++ Queue/Queue Reversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void reverseQueue(queue<int> &q) {
+        // code here
+        stack<int> st;
+        while (!q.empty()) {
+            st.push(q.front());
+            q.pop();
+        }
+        while (!st.empty()) {
+            q.push(st.top());
+            st.pop();
+        }
+    }
+};
