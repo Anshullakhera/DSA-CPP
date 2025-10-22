23887f5aef7a742c9929470bf9a8271a38a70c76
Anshullakhera <78374334+Anshullakhera@users.noreply.github.com>
2025-10-22 18:53:20 +0530

    Implement nearly sorted algorithm using priority queue

---
+++ Sorting/Nearly sorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int n = arr.size();
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            pq.push(arr[i]);
+            
+            if (pq.size() > k) {
+                arr[index++] = pq.top();
+                pq.pop();
+            }
+        }
+        
+        while (!pq.empty()) {
+            arr[index++] = pq.top();
+            pq.pop();
+        }
+    }
+};
